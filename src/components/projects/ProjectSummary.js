8cdae5a397352906b60302b7520b85ebcf5bcae8
Alejandro Roa <alejandro@groowly.com>
2020-04-09 15:57:28 -0400

    add dummy data

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title ">Project title</span>
-        <p>Posted by Alejo</p>
-        <p className="grey-text">08/04/2020</p>
+        <span className="card-title ">{project.title}</span>
+        <p> {project.posted} </p>
+        <p className="grey-text"> {project.date} </p>
       </div>
     </div>
   )
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -8,5 +8,3 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer
-
-// the key name will be the data property on the state object
