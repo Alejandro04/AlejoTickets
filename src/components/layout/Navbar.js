76702e66fb42f9b8239e07400ccb35197db883fc
Alejandro Roa <alejodev04@gmail.com>
2020-04-10 13:24:15 -0400

    show menu options if the user is logged or not

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,19 +6,20 @@ import { connect } from 'react-redux'
 
 const Navbar = (props) => {
   const { auth } = props;
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to='/' className="brand-logo">Alejo Tickets</Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   )
 }
 
 const mapStateToProps = (state) => {
-  return{
+  return {
     auth: state.firebase.auth
   }
 }
